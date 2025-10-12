3c20241ef90ce4d1793215d7d73c88a15f9ad7ef
laris <laris@DESKTOP-BQHQKST>
2025-10-12 22:41:41 +0300

    Add project files.

---
+++ Mot_Larisa_Lab2/Data/Mot_Larisa_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mot_Larisa_Lab2.Models;
+
+namespace Mot_Larisa_Lab2.Data
+{
+    public class Mot_Larisa_Lab2Context : DbContext
+    {
+        public Mot_Larisa_Lab2Context (DbContextOptions<Mot_Larisa_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mot_Larisa_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Mot_Larisa_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Mot_Larisa_Lab2/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Mot_Larisa_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
+    }
+}
